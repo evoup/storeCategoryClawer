879dd75b7bc8c8c6826fd247d315d7070d1a92c5
evoup <evoex123@gmail.com>
2017-08-09 08:56:31 +0000

    c

--- clawerAndroid.php
+++ clawerAndroid.php
@@ -267,7 +267,7 @@ $proxy = explode(':', $proxy);
 $storeUrl="https://play.google.com/store/apps";
 $userAgent = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)';
 
-$languageCode=array('zh'=>'121');
+//$languageCode=array('zh'=>'121');
 foreach (array_keys($languageCode) as $code) {
     grab($storeUrl,$userAgent,$proxy,$code);
 }
