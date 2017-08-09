e18699bfb053a0cbb3bd69e5f6f2f09923b1ab51
evoup <evoex123@gmail.com>
2017-08-09 10:38:19 +0000

    c

--- saveDb.php
+++ saveDb.php
@@ -23,7 +23,7 @@
  *  UNIQUE KEY `category_id` (`ios_app_category_id`),
  *  KEY `parenet_id` (`parent_id`),
  *  KEY `category_parent_id` (`ios_app_category_id`,`parent_id`)
- *) ENGINE=InnoDB AUTO_INCREMENT=3318 DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+ *) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
  */
 
 $CAT=unserialize(file_get_contents('/tmp/res.json'));
